9f67766ba752ac535d4ab26107518ac2671f1cdb
mrbaker1917 <mrbaker1917@gmail.com>
2026-08-10 17:16:57 -0700

    most of login/reg users done

--- src/commands/users.ts
+++ src/commands/users.ts
@@ -1,6 +1,7 @@
 import { setUser } from "../config";
+import { createUser, getUserByName } from "../lib/db/queries/users";
 
-export function handlerLogin(cmdName: string, ...args: string[]) {
+export async function handlerLogin(cmdName: string, ...args: string[]) {
   if (args.length !== 1) {
     throw new Error(`usage: ${cmdName} <name>`);
   }
@@ -8,4 +9,20 @@ export function handlerLogin(cmdName: string, ...args: string[]) {
   const userName = args[0];
   setUser(userName);
   console.log("User switched successfully!");
+}
+
+export async function handlerRegister(cmdName: string, ...args: string[]) {
+  if (args.length !== 1) {
+    throw new Error(`usage: ${cmdName} <name>`);
+  }
+
+  const userName = args[0];
+  const existingUser = await getUserByName(userName);
+  if (existingUser) {
+    throw new Error(`User with name "${userName}" already exists.`);
+  }
+
+  await createUser(userName);
+  setUser(userName);
+  console.log("User registered successfully!");
 }
