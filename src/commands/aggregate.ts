068da1dae17fc4ace914a0d8db307540358bd2c3
mrbaker1917 <mrbaker1917@gmail.com>
2026-08-17 15:46:50 -0700

    adds help functions for getting new feeds

--- src/commands/aggregate.ts
+++ src/commands/aggregate.ts
@@ -1,8 +1,22 @@
 import { fetchFeed } from "../lib/rss.js";
+import { markFeedFetched, getNextFeedToFetch } from "src/lib/db/queries/feeds.js";
 
-export async function handlerAgg(_: string) {
+export async function handlerAgg(time_between_reqs: string) {
     const feedURL = "https://www.wagslane.dev/index.xml";
     const feedData = await fetchFeed(feedURL);
-    const feedDataStr = JSON.stringify(feedData)
+    const feedDataStr = JSON.stringify(feedData);
     console.log(feedDataStr);
+}
+
+async function scrapeFeeds() {
+    const nextFeed = await getNextFeedToFetch();
+    if (!nextFeed) {
+        console.log("No feeds to fetch.");
+        return;
+    }
+    const feedData = await fetchFeed(nextFeed.url);
+    const fetchedFeed = await markFeedFetched(nextFeed.id);
+    for (let item of feedData.channel.item) {
+        console.log(item.title);
+    }
 }
--- src/lib/db/queries/feeds.ts
+++ src/lib/db/queries/feeds.ts
@@ -1,6 +1,6 @@
 import { db } from "..";
 import { feeds } from "../schema";
-import { eq } from "drizzle-orm";
+import { eq, sql } from "drizzle-orm";
 
 
 export async function createFeed(name: string, url: string, userId: string) {
@@ -15,4 +15,18 @@ export async function getFeeds() {
 export async function getFeedByUrl(url: string) {
     const [feed] = await db.select().from(feeds).where(eq(feeds.url, url));
     return feed;
+}
+
+export async function markFeedFetched(feedId: string) {
+  const [result] = await db.update(feeds)
+  .set({ lastFetchedAt: new Date(), updatedAt: new Date()})
+  .where(eq(feeds.id, feedId)).returning();
+  return result;
+}
+
+export async function getNextFeedToFetch() {
+  const [result] = await db.select().from(feeds)
+  .orderBy(sql `${feeds.lastFetchedAt} asc nulls first`)
+  .limit(1);
+  return result;
 }
