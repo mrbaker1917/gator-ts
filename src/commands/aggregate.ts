4c80fc8e92e75c37293c72305eb81e7deaa87dd6
mrbaker1917 <mrbaker1917@gmail.com>
2026-08-17 17:01:59 -0700

    changes handlerAgg()

--- src/commands/aggregate.ts
+++ src/commands/aggregate.ts
@@ -1,11 +1,24 @@
 import { fetchFeed } from "../lib/rss.js";
 import { markFeedFetched, getNextFeedToFetch } from "src/lib/db/queries/feeds.js";
 
-export async function handlerAgg(time_between_reqs: string) {
-    const feedURL = "https://www.wagslane.dev/index.xml";
-    const feedData = await fetchFeed(feedURL);
-    const feedDataStr = JSON.stringify(feedData);
-    console.log(feedDataStr);
+export async function handlerAgg(cmdName: string, ...args: string[]) {
+    if (args.length < 1) {
+        console.log("usage: cli agg <time_between_reqs>");
+        process.exit(1);
+    }
+    const time_between_reqs = args[0];
+    const interval = parseDuration(time_between_reqs);
+    console.log(`Starting feed scraping every ${interval} milliseconds...`);
+    scrapeFeeds().catch((err) => {
+        console.error(`Error scraping feeds: ${err}`);
+        process.exit(1);
+    });
+    setInterval(() => {
+        scrapeFeeds().catch((err) => {
+            console.error(`Error scraping feeds: ${err}`);
+            process.exit(1);
+        });
+    }, interval);  
 }
 
 async function scrapeFeeds() {
@@ -19,4 +32,27 @@ async function scrapeFeeds() {
     for (let item of feedData.channel.item) {
         console.log(item.title);
     }
+}
+
+function parseDuration(duration: string): number {
+    const regex = /(\d+)([smhd])/g;
+    let milliSeconds = 0;
+    let match;
+
+    while ((match = regex.exec(duration)) !== null) {
+        const value = parseInt(match[1], 10);
+        const unit = match[2];
+
+        switch (unit) {
+            case 's':
+                milliSeconds += value * 1000;
+            case 'm':
+                milliSeconds += value * 60 * 1000;
+            case 'h':
+                milliSeconds += value * 60 * 60 * 1000;
+            case 'd':
+                milliSeconds += value * 24 * 60 * 60 * 1000;
+        }
+    }
+    return milliSeconds;
 }
